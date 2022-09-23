491c6a79f817d134b3c233e859bcfbc8383ea9c3
Insu Hong <hinsu93@gmail.com>
2022-09-23 14:22:57 +0900

    라우트 설정해서 페이지 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
+import Home from "./Home";
 
 function App() {
   return (
-    <div>
-      <Login />
-      <Signup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
